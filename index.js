e604e6863958212c779c598c691dd2d86d0c17e4
Lucas Gazzola <85223876+lucasjg017@users.noreply.github.com>
2021-09-23 11:48:20 -0300

    feat: add testing route

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
-require('dotenv').config();
-require('./mongo');
-const express = require('express');
-const logger = require('./loggerMiddleware');
-const cors = require('cors');
-const notFound = require('./middlewares/notFound');
-const handleError = require('./middlewares/handleError');
-const usersRouter = require('./controllers/users');
-const notesRouter = require('./controllers/notes');
-const loginRouter = require('./controllers/login');
+require('dotenv').config()
+require('./mongo')
+const express = require('express')
+const logger = require('./loggerMiddleware')
+const cors = require('cors')
+const notFound = require('./middlewares/notFound')
+const handleError = require('./middlewares/handleError')
+const usersRouter = require('./controllers/users')
+const notesRouter = require('./controllers/notes')
+const loginRouter = require('./controllers/login')
 
-const app = express();
-app.use(express.json());
-app.use(logger);
-app.use(cors());
-app.use('/', loginRouter);
-app.use('/', notesRouter);
-app.use('/', usersRouter);
-app.use(notFound);
-app.use(handleError);
+const app = express()
+app.use(express.json())
+app.use(logger)
+app.use(cors())
+app.use('/', loginRouter)
+app.use('/', notesRouter)
+app.use('/', usersRouter)
 
-const PORT = process.env.PORT;
-const server = app.listen(PORT, () => { console.log(`Se ha realizado con éxito en el puerto ${PORT}`) });
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controllers/testing')
+  app.use('/api/testing', testingRouter)
+}
 
-module.exports = { app, server };
+app.use(notFound)
+app.use(handleError)
+
+const PORT = process.env.PORT
+const server = app.listen(PORT, () => { console.log(`Se ha realizado con éxito en el puerto ${PORT}`) })
+
+module.exports = { app, server }
